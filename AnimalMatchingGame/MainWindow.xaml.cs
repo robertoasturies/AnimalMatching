060e6cc5f0c4d49fb636339938106989d3a49224
Roberto Delgado Sánchez <roberto.asturies@gmail.com>
2020-12-06 10:59:52 +0100

    First steps to create the app: SetUpGame method and basic layout created.

---
+++ AnimalMatchingGame/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimalMatchingGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmojis = new List<string>()
+            {
+                "🐮","🐮",
+                "🦄","🦄",
+                "🐔","🐔",
+                "🐴","🐴",
+                "🐷","🐷",
+                "🐼","🐼",
+                "🐼","🐼",
+                "🐹","🐹"
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmojis.Count);
+
+                string nextEmoji = animalEmojis[index];
+
+                textBlock.Text = nextEmoji;
+
+                animalEmojis.RemoveAt(index);
+            }
+        }
+    }
+}
